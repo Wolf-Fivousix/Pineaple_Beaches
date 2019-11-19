748beadd13d36f5621ec534ed924647727fcb854
Diego Francisco <Fivousix@gmail.com>
2019-11-19 09:30:53 -0800

    Add validations in the correct folder

--- validation/beach.js
+++ validation/beach.js
@@ -7,35 +7,40 @@ module.exports = function validateBeachInput(data) {
     // NAME
     data.name = validText(data.name) ? data.name : "";
     if (!Validator.isLength(data.name, { min: 3, max: 50 })) {
-        errors.text = "Beach Name must be between 3 & 50 characters";
+        errors.name = "Beach Name must be between 3 & 50 characters";
     }
     if (Validator.isEmpty(data.name)) {
-        errors.text = "You HAVE to name it something...";
+        errors.name = "You HAVE to name it something...";
     }
 
     // DESCRIPTION
     data.description = validText(data.description) ? data.description : "";
     if (!Validator.isLength(data.description, { min: 5, max: 500 })) {
-        errors.text = "Beach must be described between 5 & 500 characters";
+        errors.description = "Beach must be described between 5 & 500 characters";
     }
     if (Validator.isEmpty(data.description)) {
-        errors.text = "You HAVE to describe something...";
+        errors.description = "You HAVE to describe something...";
     }
 
     // LOCATION
     data.location = validText(data.location) ? data.location : "";
     if (!Validator.isLength(data.location, { min: 5, max: 500 })) {
-        errors.text = "Beach location needs at least 2 characters";
+        errors.location = "Beach location needs at least 2 characters";
     }
     if (Validator.isEmpty(data.location)) {
-        errors.text = "It HAS to be located somewhere...";
+        errors.location = "It HAS to be located somewhere...";
     }
 
     // CROWDLEVEL
     // No validations.
 
     // TEMPERATURE
-    // No validations.
+    data.temperature = typeof data.temperature === "number" ? data.temperature : "";
+    // Make sure this actually worked.
+    debugger;
+    if (Validator.isEmpty(data.temperature)) {
+        errors.temperature = "It HAS a temperature, give it!";
+    }
 
     return {
         errors,
