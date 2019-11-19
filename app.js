38675f08bfa701d6e37a59d29a167a1619bf9812
claudius <claudius.solomon@gmail.com>
2019-11-19 11:55:47 -0800

    Finished UserAuth

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const passport = require('passport');
 const app = express();
 const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
@@ -15,12 +16,13 @@ mongoose
 app.use(bodyParser.urlencoded({
     extended: false
 }));
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
 app.use(bodyParser.json());
 app.get("/", (request, response) => {
     const user = new User({
-        handle: "Wolf",
-        email: "wolfy",
+        username: "wolfy",
         password: "123"
     });
     user.save();
--- validation/login.js
+++ validation/login.js
@@ -3,14 +3,11 @@ const validText = require("./valid-text");
 
 module.exports = function (data) {
     let errors = {};
-    data.email = validText(data.email) ? data.email : "";
+    data.username = validText(data.username) ? data.username : "";
     data.password = validText(data.password) ? data.password : "";
 
-    if (!Validator.isEmail(data.email)) {
-        errors.email = "Email is invalid";
-    }
-    if (!Validator.isEmpty(data.email)) {
-        errors.email = "Email is required";
+    if (Validator.isEmpty(data.username)) {
+        errors.username = "Username is required";
     }
     if(Validator.isEmpty(data.password)) {
         errors.password = "Password is required";
