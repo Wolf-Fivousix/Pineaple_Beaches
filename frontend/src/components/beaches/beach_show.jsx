a5d58940a78e30f87e31ed49cb986225a86997a1
App Academy Student <student@appacademy.io>
2019-11-21 17:33:12 -0800

    Make API call prototype functional

--- frontend/src/components/beaches/beach_show.jsx
+++ frontend/src/components/beaches/beach_show.jsx
@@ -1,13 +1,30 @@
 import React from "react";
+import axios from 'axios';
 
 class BeachShow extends React.Component {
     componentDidMount() {
         console.log("mounted");
     }
     
+    updateWeatherData() {
+        console.log("Updating data...");
+        axios.get("http://api.openweathermap.org/data/2.5/weather?lat=37.771267&lon=-122.512803&appid=")
+            .then(response => console.log(response))
+            .catch(errors => console.log(errors));
+    }
+
+
     render() {
         return (
-            <p>This is a beach page, yay! =)</p>
+            <div>
+                <br/>
+                <br/>
+                <br/>
+                <p>This is a beach page, yay! =)</p>
+                <button className="signup-but"
+                    onClick={this.updateWeatherData.bind(this)}
+                >CLICK MEEEEE =)</button>
+            </div>
         );
     }
 };
