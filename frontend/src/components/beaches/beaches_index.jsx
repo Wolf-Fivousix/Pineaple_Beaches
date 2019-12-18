fdfc8b87f07092a0ca1310e62dc635dc2910bb74
Diego Francisco <Fivousix@gmail.com>
2019-12-18 12:59:20 -0800

    Remove commented out code

--- frontend/src/components/beaches/beaches_index.jsx
+++ frontend/src/components/beaches/beaches_index.jsx
@@ -1,22 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import BeachShow from "./beach_show";
-// import { connect } from "react-redux";
 
 class BeachesIndex extends React.Component {
-  // handleClick() {
-  // 	return (e) => console.log(e.currentTarget);
-  // }
 
   render() {
     const { beach } = this.props;
     const beachName = beach.name;
-    // console.log(new Date().getTime() - Date.now());
-    // console.log(typeof (new Date()));
-    // const timeDifference = (Date.now() - new Date(beach.date).getTime()) / 1000 / 60;
-    // const time = Math.floor(timeDifference);
-    // console.log(new Date(beach.date).getTime());
-    // console.log(ObjectId(beach._id).getTimestamp());
+
     return (
       <Link to={`/beaches/${beach._id}`}>
         <div className="picTitleAligner">
@@ -35,5 +25,4 @@ class BeachesIndex extends React.Component {
   }
 }
 
-// export default connect(mapStateToProps, mapDispatchToProps)(BeachesIndex);
 export default BeachesIndex;
