04c39ca384ecdb0a5e56183fd9b92462968673c5
Diego Francisco <Fivousix@gmail.com>
2019-12-18 12:55:25 -0800

    Add class to indexing

--- frontend/src/components/beaches/beaches_index.jsx
+++ frontend/src/components/beaches/beaches_index.jsx
@@ -21,7 +21,7 @@ class BeachesIndex extends React.Component {
       <Link to={`/beaches/${beach._id}`}>
         <div className="picTitleAligner">
           <img src={require(`../../images/${beachName}.jpg`)}  alt="beachImage" className="miniImage"/>
-          <ul>
+          <ul className="beachIndexInfo">
             <div className="beach-name-container">
               <li><h1 className="beach-name">{beach.name}</h1></li>
             </div>
